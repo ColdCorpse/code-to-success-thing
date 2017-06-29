969bc35d1a951ec4c769234562008f03080c1bcb
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 16:16:31 +0000

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,22 +4,35 @@ class PortfoliosController < ApplicationController
   end
   
   def new
-  @portfolio_item = Portfolio.new
+    @portfolio_item = Portfolio.new
   end
   
-  
-    def create
- @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
-      if@portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'you did the thing
-      .' }
-        format.json { render :show, status: :created, location: @blog }
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity}  
       end
     end
-end 
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+   def update
+      @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html {redirect_to portfolios_path, notice: 'you edited the thing
+        .' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
