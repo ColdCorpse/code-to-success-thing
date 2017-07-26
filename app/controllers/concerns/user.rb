1cc04af5ac97d566cccf77f26c51c93eb7a3f774
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 15:31:58 +0000

    Added session tracker for dynamic source links

--- app/controllers/concerns/user.rb
+++ app/controllers/concerns/user.rb
@@ -3,6 +3,16 @@ class User <ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
+  
+  
+    before_create :configure_permitted_parameters, if: :devise_controller?
+  
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  devise_parameter_sanitizer.permit(:account_update,keys: [:name])
+  end
+
+
   validates_presence_of :name
   
   def first_name 
