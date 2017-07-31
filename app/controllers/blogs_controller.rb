5afef705f1112afab7cd7d0f8b93788dd317c193
Jordan Hudgens <jordan@devcamp.com>
2017-07-31 17:48:38 +0000

    Added navigation bar and added default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,14 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title =" My Porftolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+     @page_title = @blog.title
+     @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
@@ -66,7 +69,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
